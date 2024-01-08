d5712c652c919f4d0faffb960bd44beeb80455ac
yogeshsingh-11 <116149692+yogeshsingh-11@users.noreply.github.com>
2024-01-08 15:43:18 +0530

    Updating imu_sensor_code.ino
    Updating imu_sensor_code.ino for printing the data coming from imu

--- Data/imu_sensor_code.ino
+++ Data/imu_sensor_code.ino
@@ -23,4 +23,20 @@ void setup(void)
   bno.setExtCrystalUse(true);
 }
 
-
+void loop(void) 
+{
+  /* Get a new sensor event */ 
+  sensors_event_t event; 
+  bno.getEvent(&event);
+  
+  /* Display the floating point data */
+  Serial.print("X: ");
+  Serial.print(event.orientation.x, 4);
+  Serial.print("\tY: ");
+  Serial.print(event.orientation.y, 4);
+  Serial.print("\tZ: ");
+  Serial.print(event.orientation.z, 4);
+  Serial.println("");
+  
+  delay(100);
+}
