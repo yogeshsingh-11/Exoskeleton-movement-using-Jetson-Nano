992373a50cab0598d2a0efb5d0275cabf48091e3
yogeshsingh-11 <116149692+yogeshsingh-11@users.noreply.github.com>
2024-01-08 15:42:24 +0530

    Create imu_sensor_code.ino
    Creating Imu-sensor-code.ino for measuring the data from BNO55 imu sensor.

---
+++ Data/imu_sensor_code.ino
@@ -0,0 +1,26 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+  
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
+void setup(void) 
+{
+  Serial.begin(9600);
+  Serial.println("Orientation Sensor Test"); Serial.println("");
+  
+  /* Initialise the sensor */
+  if(!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+  
+  delay(1000);
+    
+  bno.setExtCrystalUse(true);
+}
+
+
